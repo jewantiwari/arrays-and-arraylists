61c5cf6c6a104a6b314cbe9677e010e95e813442
Jack Tiwari <jack.jewan@gmail.com>
2023-06-08 11:12:41 -0400

    Practice MovieList

--- src/arrays_and_arraylists/ArrayListsPractice.java
+++ src/arrays_and_arraylists/ArrayListsPractice.java
@@ -1,17 +1,48 @@
 package arrays_and_arraylists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayListsPractice {
     public static void main(String[] args) {
 //        Create an ArrayList
+        ArrayList<String> movieList = new ArrayList<>();
+
+                movieList.add("Titanic");
+                movieList.add("The GodFather");
+                movieList.add("The Dark Knight");
+                movieList.add("Fight Club");
+                movieList.add("Pulp Fiction");
+
 //        add 5 elements to it
 //        print the entire ArrayList to the terminal
+        System.out.println(movieList);
+
 //        print the length of it
+        System.out.println(movieList.size());
 //        remove some elements from the ArrayList
+        movieList.remove("Fight Club");
+
 //        print the entire ArrayList again
+        System.out.println(movieList);
 //        change some values in the ArrayList
+        movieList.set(4, "Terminator");
 //        print it again to see your changes
+        System.out.println(movieList);
 
 //        Create an ArrayList with 5 elements and give the value in-line (you will need to use Arrays.asList())
+
+                // Create an ArrayList of popular business shows
+                ArrayList<String> businessShows = new ArrayList<>(Arrays.asList(
+                        "Suits",
+                        "Billions",
+                        "The Office",
+                        "Silicon Valley",
+                        "Mad Men"
+                ));
+                for (String show: businessShows){
+                System.out.println(show);}
+            }
 
 //        mess around with a couple methods for ArrayLists. Just type out the ArrayList variable name you made and then put a "." and scroll through the list of methods.
 //        read the information it shows you and try to figure out how to use it.
@@ -21,8 +52,8 @@ public class ArrayListsPractice {
 //        }
 //        print out whatever you need to the terminal to check and see if the methods work the way you think they should.
 
-        System.out.println("Print something from arraylists practice");
+
 
 
     }
-}
+
--- src/arrays_and_arraylists/ArraysPractice.java
+++ src/arrays_and_arraylists/ArraysPractice.java
@@ -1,17 +1,31 @@
 package arrays_and_arraylists;
 
+import java.util.Arrays;
+
 public class ArraysPractice {
     public static void main(String[] args) {
 //        Create an empty array with 5 elements
+        String[] favoriteMovies =new String[5];
 //        give each index in the array a value
+        favoriteMovies[0]= "Titanic";
+        favoriteMovies[1]= "The West";
+        favoriteMovies[2]= "Tiger";
+        favoriteMovies[3]= "Thappad";
+        favoriteMovies[4]= "TheaterOne";
 //        print the entire array to the terminal (you will need to use the Array.toString() method)
+        System.out.println(Arrays.toString(favoriteMovies));
 //        print the length of the array
-
+        System.out.println(favoriteMovies.length);
 //        Create an array with 5 elements and give the values in-line
+        int[] favNums ={7,11,23,27,8,9,31};
 //        print the entire array to the terminal
+        System.out.println(Arrays.toString(favNums));
 //        print the length of the array
-
+        System.out.println(favNums.length);
 //        change a few values in each array and then print both arrays to the terminal again
+        favNums[2]= 90;
+
+        System.out.println(Arrays.toString(favNums));
         System.out.println("Hello World");
     }
 
